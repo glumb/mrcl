de7543525d4e6187744b3106a1dd66742dc6aa8a
Maximilian Beck <akkumulator1@gmail.com>
2017-07-13 10:54:55 +0200

    feat(examples): added list view

--- examples/StatusHelper.js
+++ examples/StatusHelper.js
@@ -1,45 +1,100 @@
 import Table from 'cli-table'
-
+import Colors from './colors'
 
 export default class StatusHelper {
-  constructor(mrcl, fullsize = true) {
-    let table
-    if (fullsize) {
-      table = new Table({
-        head: ['MRIL', 'sending', 'sent', 'executing', 'executed'],
-        colWidths: [70, 15, 15, 15, 15],
-      })
+  constructor(mrcl, view = 'status') {
+    switch (view) {
+      case 'fullsize': {
+        this.instructionStatusTable = new Table({
+          head: ['MRIL', 'sending', 'sent', 'executing', 'executed'],
+          colWidths: [70, 15, 15, 15, 15],
+        })
+      }
+      case 'status':
+        {
+          this.queueStatusTable = new Table({
+            head: ['queue size', 'sent commands', 'freeReceiveBuffer'],
+            colWidths: [15, 15, 15],
+          })
+        }
+        break
+      default:
+
     }
 
-    const MRCLTable = new Table({
-      head: ['queue size', 'sent commands', 'freeReceiveBuffer'],
-      colWidths: [15, 15, 15],
-    })
+    // DISPLAY MODES
 
-    function displayMRCLTable() {
-      MRCLTable.splice(0, MRCLTable.length)
+    const displayQueueStatus = () => {
+      this.queueStatusTable.splice(0, this.queueStatusTable.length)
 
-      MRCLTable.push([mrcl.getCommandsQueue().length, mrcl.getSentCommands().length, mrcl.getFreeReceiveBuffer()])
+      this.queueStatusTable.push([mrcl.getCommandsQueue().length, mrcl.getSentCommands().length, mrcl.getFreeReceiveBuffer()])
 
-      console.log(MRCLTable.toString())
+      console.log(this.queueStatusTable.toString())
     }
 
-    function displayCommandTable() {
-      table.splice(0, table.length)
+    const displayInstuctionStatus = () => {
+      this.instructionStatusTable.splice(0, this.instructionStatusTable.length)
 
       mrcl.getCommands().forEach((c) => {
-        table.push([c.getMRIL().getRawInstruction(), (c.isSending()) ? 'x' : '', (c.isSent()) ? 'x' : '', (c.getMRIL().isExecuting()) ? 'x' : '', (c.getMRIL().isExecuted()) ? 'x' : ''])
+        this.instructionStatusTable.push([c.getMRIL().getRawInstruction(), (c.isSending()) ? 'x' : '', (c.isSent()) ? 'x' : '', (c.getMRIL().isExecuting()) ? 'x' : '', (c.getMRIL().isExecuted()) ? 'x' : ''])
       })
 
-      console.log(table.toString())
+      console.log(this.instructionStatusTable.toString())
     }
 
+    const displayList = () => {
+      const numberOfLinesToDisplay = process.stdout.rows
+      const numberOfExecutedLines = Math.floor((numberOfLinesToDisplay - 1) / 2)
+      const numberOfFutureLines = Math.floor((numberOfLinesToDisplay - 1) / 2)
+      const commands = mrcl.getCommands()
+      let lastExecutedInstruction = 0
+      for (let i = 0; i < commands.length; i++) {
+        if (commands[i].getMRIL().isExecuted()) {
+          lastExecutedInstruction = i
+          if (i + 1 < commands.length && !commands[i + 1].getMRIL().isExecuted()) {
+            break
+          }
+        }
+      }
+
+      // const startIndex = Math.max(lastExecutedInstruction - numberOfExecutedLines + 1, 0)
+      // const endIndex = Math.min(lastExecutedInstruction + numberOfFutureLines + 2, commands.length)
+      // const lines = commands.slice(startIndex, endIndex)
+      const startIndex = Math.max(Math.min(lastExecutedInstruction - numberOfExecutedLines + 1, commands.length - numberOfLinesToDisplay), 0)
+      const endIndex = Math.min(startIndex + numberOfLinesToDisplay - 1, commands.length)
+      const lines = commands.slice(startIndex, endIndex)
+
+      for (const line of lines) {
+        let color = Colors.Reset
+        if (line.getMRIL().isExecuted()) {
+          color = Colors.Reset
+        } else if (line.getMRIL().isExecuting()) {
+          color = Colors.FgGreen
+        } else if (line.isSent()) {
+          color = Colors.FgYellow
+        }
+
+        // pad the line with spaces to not have to reset the terminal (performance)
+        console.log(color, line.getMRIL().getRawInstruction(), ' '.repeat(process.stdout.columns - line.getMRIL().getRawInstruction().length - 3), Colors.Reset)
+      }
+    }
+
+    // clear screen
     console.log('\x1Bc')
-    function display() {
+
+    const display = () => {
       process.stdout.write('\x1B[1;1f') // reset cursor
-      displayMRCLTable()
-      if (fullsize) {
-        displayCommandTable()
+      switch (view) {
+        case 'fullsize':
+          displayInstuctionStatus()
+        case 'status':
+          displayQueueStatus()
+          break
+        case 'list':
+          displayList()
+          break
+        default:
+          displayList()
       }
     }
 
@@ -56,4 +111,6 @@ export default class StatusHelper {
       display()
     })
   }
+
+
 }
